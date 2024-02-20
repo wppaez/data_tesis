223a62b61d16832270e87af1c4dfd2190668665e
Cristhyan D. Marchena <crdemar@gmail.com>
2024-02-19 23:54:11 -0500

    wip(app): trying to scrape comments

--- src/helpers.py
+++ src/helpers.py
@@ -2,6 +2,8 @@ import asyncio
 from typing import Literal
 from playwright.async_api import Page
 
+NAVIGATION_DELAY = 10000
+
 MESSAGES = {
     "NOT_NOW": "El cuadro de diálogo 'Ahora no' no estaba presente o no se pudo cerrar.",
     "NOTIFICATIONS": "El cuadro de diálogo 'Activar notificaciones' no estaba presente o no se pudo cerrar.",
@@ -29,7 +31,6 @@ async def close_not_now(
         not_now_is_visible = await page.get_by_role(
             "button", name="Not Now"
         ).is_visible()
-        
 
         if not_now_is_visible:
             await page.get_by_role("button", name="Not Now").click()
@@ -40,3 +41,70 @@ async def close_not_now(
 
     finally:
         return
+
+
+async def login(page: Page, username: str, password: str):
+    # Navegar a Instagram e iniciar sesión
+    await page.goto("https://www.instagram.com/")
+    await page.fill("input[name='username']", username)
+    await page.fill("input[name='password']", password)
+    await page.click("button[type='submit']")
+    await page.wait_for_timeout(NAVIGATION_DELAY)
+
+    # Cerrar diálogos
+    await close_not_now(page)
+    await page.wait_for_timeout(NAVIGATION_DELAY)
+
+    await close_not_now(page, "NOTIFICATIONS")
+    return
+
+
+async def go_to_profile(page: Page, profile: str):
+    # Buscar
+    await page.get_by_role("link", name="Search Search").click()
+    await page.wait_for_timeout(NAVIGATION_DELAY)
+    await page.get_by_placeholder("Search").click()
+    await page.get_by_placeholder("Search").fill(profile)
+    await page.keyboard.press("Enter")
+
+    # Ir al perfil
+    await page.click(f"a[href='/{profile}/']")
+    await page.wait_for_timeout(NAVIGATION_DELAY)
+
+    # Ir a la sección de publicaciones (posts)
+    await page.get_by_role("tab", name="Posts").click()
+    await page.wait_for_timeout(NAVIGATION_DELAY)
+    return
+
+
+async def get_profile_posts(page: Page) -> list[str]:
+    # Obtener el número total de posts
+    total_posts_text = await page.locator(
+        '[role="main"] section ul :nth-child(1)'
+    ).first.text_content()
+    total_posts = int((total_posts_text or "").replace("posts", ""))
+    print(f"Total number of posts: {total_posts}")
+
+    # Extraer todos los posts
+    posts = await page.locator('[role="main"] [role="tablist"] ~ div a').count()
+    hrefs = []
+    while posts < total_posts:
+        links_query = '[role="main"] [role="tablist"] ~ div a'
+        links = await page.locator(links_query).all()
+        href_cache = [await link.get_attribute("href") for link in links[-12:]]
+        hrefs.extend(href_cache)
+        await page.keyboard.press("End")
+        await page.wait_for_timeout(3000)
+        posts += 12
+        print(posts, total_posts, len(hrefs))
+
+    links = await page.locator('[role="main"] [role="tablist"] ~ div a').all()
+    remaining = total_posts - len(hrefs)
+    href_cache = [await link.get_attribute("href") for link in links[-remaining:]]
+    hrefs.extend(href_cache)
+
+    # Completar el link de los posts
+    profile_url = page.url
+    links_to_visit = [f"{profile_url}/{href}".replace("///", "/") for href in hrefs]
+
+    return links_to_visit
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import asyncio
 import traceback
 from playwright.async_api import async_playwright
 from dotenv import load_dotenv
-from helpers import close_not_now
+from helpers import login, go_to_profile, get_profile_posts
 
 load_dotenv()
 
@@ -11,71 +11,39 @@ INSTAGRAM_USER = "data.user_wppb"
 INSTAGRAM_PASSWORD = "Cliford99"
 NAVIGATION_DELAY = 10000
 
+
 async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
-        context = await browser.new_context(locale='en-US')
+        context = await browser.new_context(locale="en-US")
         page = await context.new_page()
 
         try:
-            # Navegar a Instagram e iniciar sesión
-            await page.goto("https://www.instagram.com/")
-            await page.fill("input[name='username']", INSTAGRAM_USER)
-            await page.fill("input[name='password']", INSTAGRAM_PASSWORD)
-            await page.click("button[type='submit']")
-            await page.wait_for_timeout(NAVIGATION_DELAY)
+            # Iniciar sesión
+            await login(page, INSTAGRAM_USER, INSTAGRAM_PASSWORD)
 
-            # Cerrar diálogos
-            await close_not_now(page)
-            await page.wait_for_timeout(NAVIGATION_DELAY)
+            # Ir a un perfil
+            await go_to_profile(page, "rappicolombia")
 
-            await close_not_now(page, "NOTIFICATIONS")
+            # Obtener links de los posts
+            links_to_visit = await get_profile_posts(page)
 
-            # Buscar un perfil
-            await page.get_by_role("link", name="Search Search").click()
-            await page.wait_for_timeout(NAVIGATION_DELAY)
+            link_to_visit = links_to_visit[0]
+            is_reel = "/reel/" in link_to_visit
+            post_tab = await context.new_page()
+            await post_tab.goto(link_to_visit)
+            await post_tab.wait_for_timeout(NAVIGATION_DELAY)
+            await post_tab.wait_for_timeout(NAVIGATION_DELAY)
+            comments_wrapper = await page.locator(
+                '[role="main"] [role="presentation"] [role="presentation"] ul > [role="button"]'
+            ).all()
+            print(len(comments_wrapper))
+            # caption = comments_wrapper[0] # for some reason doesn't work
+            await post_tab.pause()
 
-            profile_id = "rappicolombia"
-            await page.get_by_placeholder("Search").click()
-            await page.get_by_placeholder("Search").fill(profile_id)
-            await page.keyboard.press("Enter")
-
-            # Ir al perfil
-            await page.click(f"a[href='/{profile_id}/']")
-            await page.wait_for_timeout(NAVIGATION_DELAY)
-
-            # Ir a la sección de publicaciones (posts)
-            await page.get_by_role("tab", name="Posts").click()
-            await page.wait_for_timeout(NAVIGATION_DELAY)
-
-            # Obtener el número total de posts
-            total_posts = await page.evaluate(
-                """() => {
-                    const totalPostsElement = document.querySelector("span._ac2a span.html-span");
-                    return totalPostsElement ? parseInt(totalPostsElement.innerText) : 0;
-                }"""
-            )
-            print(f"Total number of posts: {total_posts}")
-
-            # Hacer clic en cada post visible y extraer comentarios
-            posts = await page.locator('[role="main"] [role="tablist"] ~ div a').count()
-            links_to_visit = []
-            while posts < total_posts:
-                links = await page.locator('[role="main"] [role="tablist"] ~ div a').all()
-                hrefs = [await link.get_attribute('href') for link in links[-12:]]
-                links_to_visit.extend(hrefs)
-                await page.keyboard.press("End")
-                await page.wait_for_timeout(3000)
-                posts += 12
-                print(posts,total_posts, len(links_to_visit))
-            
-            links = await page.locator('[role="main"] [role="tablist"] ~ div a').all()
-            remaining = posts - total_posts 
-            hrefs = [await link.get_attribute('href') for link in links[-remaining:]]
-            links_to_visit.extend(hrefs)
             print(links_to_visit)
-                
-            
+            print(len(links_to_visit))
+
             # Cerrar sesión
             await page.goto("https://www.instagram.com/accounts/logout/")
             await page.wait_for_timeout(NAVIGATION_DELAY)
@@ -84,6 +52,7 @@ async def main():
             traceback.print_exc()
         finally:
             await browser.close()
+
 
 # Ejecuta la función principal
 asyncio.run(main())
